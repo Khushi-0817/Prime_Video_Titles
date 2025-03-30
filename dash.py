e20b7d28cb4161267a395c2fe18db13fddfc2555
Khushi-0817 <152060329+Khushi-0817@users.noreply.github.com>
2025-03-30 20:38:11 +0530

    Update dash.py

--- dash.py
+++ dash.py
@@ -2,10 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 df = pd.read_csv("C:\\Users\\hp\\Downloads\\amazon_prime_titles.csv")
+#display the dataset information
 df.info()
 df.describe()
 df.drop_duplicates(inplace=True)
 df.dropna(subset=['release_year', 'rating', 'country', 'listed_in'], inplace=True)
+
 
 # bar chart showing top 10 most common genres
 plt.figure(figsize=(10, 6))
@@ -16,6 +18,8 @@ plt.xlabel('Genre')
 plt.ylabel('Count')
 plt.xticks(rotation=45)
 
+
+
 #plotting a line chart showing no. of titles released per year
 plt.figure(figsize=(10, 5))
 yearly_counts = df.groupby('release_year').size()
@@ -25,12 +29,16 @@ plt.xlabel('Year')
 plt.ylabel('Number of Titles')
 plt.grid()
 
+
+
 #plotting a pie chart showing top 5 content ratings distribution
 r_counts = df.groupby('rating').size().nlargest(5) 
 plt.figure(figsize=(7, 7))
 colors = sns.color_palette('pastel')
 plt.pie(r_counts, labels=r_counts.index, autopct='%1.1f%%', colors=colors)
 plt.title('Top 5 Content Ratings Distribution')
+
+
 
 #plotting a histogram showing distribution of release years
 plt.figure(figsize=(10, 5))
@@ -39,6 +47,8 @@ plt.title('Distribution of Release Years')
 plt.xlabel('Year')
 plt.ylabel('Frequency')
 plt.grid()
+
+
 
 #plotting a donut chart showing Top 5 content ratings distribution
 r_counts = df['rating'].value_counts().head(5)
