60ce480430d6555842190a1a1fd05a8fe4999a4b
Khushi-0817 <152060329+Khushi-0817@users.noreply.github.com>
2025-04-11 00:20:23 +0530

    Update dash.py

--- dash.py
+++ dash.py
@@ -5,6 +5,11 @@ df = pd.read_csv("C:\\Users\\hp\\Downloads\\amazon_prime_titles.csv")
 #display the dataset information
 df.info()
 df.describe()
+df.shape
+df.head()
+df.nunique()
+df.isnull().sum()
+df.columns
 df.drop_duplicates(inplace=True)
 df.dropna(subset=['release_year', 'rating', 'country', 'listed_in'], inplace=True)
 
@@ -20,9 +25,9 @@ plt.xticks(rotation=45)
 
 
 
-#plotting a line chart showing no. of titles released per year
+# line chart showing no. of titles released per year
 plt.figure(figsize=(10, 5))
-yearly_counts = df.groupby('release_year').size()
+yearly_counts = df.groupby('release_year').size().sort_index()
 plt.plot(yearly_counts.index, yearly_counts.values, marker='o', linestyle='-', color='red') 
 plt.title('Number of Titles Released per Year')
 plt.xlabel('Year')
@@ -31,7 +36,7 @@ plt.grid()
 
 
 
-#plotting a pie chart showing top 5 content ratings distribution
+# pie chart showing top 5 content ratings distribution
 r_counts = df.groupby('rating').size().nlargest(5) 
 plt.figure(figsize=(7, 7))
 colors = sns.color_palette('pastel')
@@ -40,7 +45,7 @@ plt.title('Top 5 Content Ratings Distribution')
 
 
 
-#plotting a histogram showing distribution of release years
+# histogram showing distribution of release years
 plt.figure(figsize=(10, 5))
 plt.hist(df['release_year'], bins=20, color='purple', edgecolor='black')  
 plt.title('Distribution of Release Years')
@@ -50,10 +55,18 @@ plt.grid()
 
 
 
-#plotting a donut chart showing Top 5 content ratings distribution
+# donut chart showing Top 5 content ratings distribution
 r_counts = df['rating'].value_counts().head(5)
 plt.figure(figsize=(7, 7))
 colors = sns.color_palette('pastel')
 plt.pie(r_counts, labels=r_counts.index, autopct='%1.1f%%', colors=colors)
 plt.gca().add_artist(plt.Circle((0, 0), 0.50, fc='white'))
 plt.title('Top 5 Content Ratings Distribution')
+
+
+# plotting a heatmap
+df['duration'] = pd.to_numeric(df['duration'].str.replace(' min', '', regex=False), errors='coerce')
+correlation_matrix = df.corr(numeric_only=True)
+plt.figure(figsize=(6, 4))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5)
+plt.title("Correlation Heatmap")
